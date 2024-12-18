8150ccbd69f364895f45d7f78a14d956e7d02f88
LYChubby <f4uzi345@gmail.com>
2024-12-18 17:21:57 +0700

    Menambah Fungsi CRUD dan Mengambil Data Jadwal

---
+++ app/src/main/java/com/example/ucp2/data/dao/JadwalDao.kt
@@ -0,0 +1,26 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface JadwalDao {
+
+    @Insert
+    suspend fun insertJadwal(jadwal: Jadwal)
+
+    @Insert
+    suspend fun updateJadwal(jadwal: Jadwal)
+
+    @Insert
+    suspend fun deleteJadwal(jadwal: Jadwal)
+
+    @Query("SELECT * FROM jadwal ORDER BY tanggal ASC")
+    fun getAllJadwal(): Flow<List<Jadwal>>
+
+    @Query("SELECT * FROM jadwal WHERE id = :id")
+    fun getJadwalById(id: String): Flow<Jadwal?>
+}

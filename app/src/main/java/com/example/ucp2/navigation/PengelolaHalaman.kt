e919f9801b3373eaea532dd635cd5ba209dede8b
LYChubby <f4uzi345@gmail.com>
2024-12-20 05:44:10 +0700

    Membuat Route Untuk Halaman Home Dan Insert Dokter

---
+++ app/src/main/java/com/example/ucp2/navigation/PengelolaHalaman.kt
@@ -0,0 +1,46 @@
+package com.example.ucp2.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.ucp2.ui.view.dokter.DestinasiInsertDokter
+import com.example.ucp2.ui.view.dokter.HomeDokterView
+import com.example.ucp2.ui.view.dokter.InsertDokterView
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route) {
+        composable(
+            route = DestinasiHome.route
+        ) {
+            HomeDokterView(
+                onCardClick = {
+//                    navController.navigate(DestinasiDetail.route)
+                },
+                onAddDokter = { navController.navigate(DestinasiInsertDokter.route)
+                },
+                modifier = modifier
+            )
+        }
+
+        composable(
+            route = DestinasiInsertDokter.route
+        ) {
+            InsertDokterView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier
+            )
+        }
+    }
+}

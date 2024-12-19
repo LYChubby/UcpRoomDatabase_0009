7c8e2331c1d973e6d2d452cecaa4b13ae51ad33e
LYChubby <f4uzi345@gmail.com>
2024-12-20 04:43:03 +0700

    Menambah Object Untuk Opsi Spesialis

---
+++ app/src/main/java/com/example/ucp2/data/Spesialis.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2.data
+
+object Spesialis {
+    val option = listOf(
+        "Dokter Umum",
+        "Dokter Gigi",
+        "Dokter Jantung",
+        "Dokter Mata",
+        "Dokter Kecantikan"
+    )
+}

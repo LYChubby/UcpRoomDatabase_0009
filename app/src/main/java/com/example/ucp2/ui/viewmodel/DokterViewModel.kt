29eb3783043d2e3dd9a0b5609b645811ed08dc7a
LYChubby <f4uzi345@gmail.com>
2024-12-20 02:04:17 +0700

    Membuat Data Class Untuk DokterEvent

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DokterViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2.ui.viewmodel
+
+data class DokterEvent(
+    val id: Int,
+    val nama: String,
+    val spesialis: String,
+    val klinik: String,
+    val telepon: String,
+    val jamKerja: String
+)

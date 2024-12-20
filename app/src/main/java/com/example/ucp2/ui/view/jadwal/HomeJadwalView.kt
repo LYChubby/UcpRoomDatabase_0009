a4150272e7fa4c4de48295821f6dd83537b9109e
LYChubby <f4uzi345@gmail.com>
2024-12-20 22:41:25 +0700

    Menambah Fungsi Untuk Body Jadwal

--- app/src/main/java/com/example/ucp2/ui/view/jadwal/HomeJadwalView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/HomeJadwalView.kt
@@ -1,8 +1,10 @@
 package com.example.ucp2.ui.view.jadwal
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,16 +14,23 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.Jadwal
+import com.example.ucp2.ui.viewmodel.JadwalUiState
+import kotlinx.coroutines.launch
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -106,5 +115,60 @@ fun ListJadwal(
                 )
             }
         )
+    }
+}
+
+@Composable
+fun BodyHomeJadwalView(
+    jadwalUiState: JadwalUiState,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        jadwalUiState.isLoading -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        jadwalUiState.isError -> {
+            LaunchedEffect(jadwalUiState.errorMessage) {
+                jadwalUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+
+        jadwalUiState.listJadwal.isEmpty() -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak Ada Data Jadwal",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            ListJadwal(
+                jadwalList = jadwalUiState.listJadwal,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = modifier
+            )
+        }
     }
 }

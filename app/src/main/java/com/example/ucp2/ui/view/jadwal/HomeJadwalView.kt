33fd7b2421477d8d83baec05f4102624c0a7268b
LYChubby <f4uzi345@gmail.com>
2024-12-20 22:52:12 +0700

    Menambah Fungsi HomeJadwalView

--- app/src/main/java/com/example/ucp2/ui/view/jadwal/HomeJadwalView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/HomeJadwalView.kt
@@ -1,35 +1,61 @@
 package com.example.ucp2.ui.view.jadwal
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.R
 import com.example.ucp2.data.entity.Jadwal
+import com.example.ucp2.ui.view.dokter.BodyHomeDokterView
+import com.example.ucp2.ui.viewmodel.JadwalAppViewModel
 import com.example.ucp2.ui.viewmodel.JadwalUiState
+import com.example.ucp2.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
 
 @OptIn (ExperimentalMaterial3Api::class)
@@ -37,13 +63,13 @@ import kotlinx.coroutines.launch
 fun CardJadwal(
     jadwal: Jadwal,
     modifier: Modifier = Modifier,
-    onClick: () -> Unit = { }
+    onDetailClick: () -> Unit
 ) {
     Card (
         modifier = modifier
             .fillMaxWidth()
             .padding(8.dp),
-        onClick = onClick
+        onClick = onDetailClick
     ) {
         Column (
             modifier = Modifier.padding(8.dp)
@@ -101,7 +127,7 @@ fun CardJadwal(
 fun ListJadwal(
     jadwalList: List<Jadwal>,
     modifier: Modifier = Modifier,
-    onClick: (String) -> Unit = { }
+    onDetailClick: (String) -> Unit
 ) {
     LazyColumn(
         modifier = modifier
@@ -111,7 +137,7 @@ fun ListJadwal(
             itemContent = { jadwal ->
                 CardJadwal(
                     jadwal = jadwal,
-                    onClick = { onClick(jadwal.id.toString()) }
+                    onDetailClick = { onDetailClick(jadwal.id.toString()) }
                 )
             }
         )
@@ -122,7 +148,7 @@ fun ListJadwal(
 fun BodyHomeJadwalView(
     jadwalUiState: JadwalUiState,
     modifier: Modifier = Modifier,
-    onClick: (String) -> Unit = { }
+    onDetailClick: (String) -> Unit
 ){
     val coroutineScope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
@@ -163,12 +189,140 @@ fun BodyHomeJadwalView(
         else -> {
             ListJadwal(
                 jadwalList = jadwalUiState.listJadwal,
-                onClick = {
-                    onClick(it)
+                onDetailClick = {
+                    onDetailClick(it)
                     println(it)
                 },
                 modifier = modifier
             )
+        }
+    }
+}
+
+@Composable
+fun HomeJadwalView(
+    viewModel: JadwalAppViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddJadwal: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier = modifier,
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .background(color = Color(0xFFFF6F61))
+                .fillMaxWidth()
+        ) {
+            // Header Section
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.fri),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(55.dp)
+                        .clip(CircleShape)
+                        .border(2.dp, Color.White, CircleShape)
+                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Hi, Fauzi Taufik",
+                        color = Color.White
+                    )
+                    Text(
+                        text = "Selamat Datang Di DisneyMedical",
+                        color = Color.White
+                    )
+                }
+                Icon(
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription = "Notification Icon",
+                    tint = Color.White
+                )
+            }
+
+            // Search Box
+            OutlinedTextField(
+                value = "",
+                onValueChange = {  },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(color = Color.White),
+                placeholder = { Text(text = "Cari Dokter") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search Icon"
+                    )
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent
+                )
+            )
+
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Button(
+                    onClick = { onAddJadwal() },
+                    modifier = Modifier.weight(0.5f)
+                        .padding(end = 5.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0A3C81))
+                ) {
+                    Text(text = "Tambah Dokter")
+                }
+                Button(
+                    onClick = { onAddJadwal() },
+                    modifier = Modifier.weight(0.5f)
+                        .padding(start = 5.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0A3C81))
+                ) {
+                    Text(text = "Lihat Jadwal")
+                }
+
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Column(
+                modifier = Modifier.fillMaxSize()
+                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                    .background(color = Color.White)
+                    .padding(16.dp)
+            ) {
+
+                Text(text = "Pilih Dokter Yang Anda Inginkan", fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp)
+                Text(text = "Dokter Kami Siap Melayani Anda",  fontSize = 13.sp,
+                    fontWeight = FontWeight.Thin)
+
+                Spacer(modifier = Modifier.size(10.dp))
+
+                // Body Content
+                val jadwalUiState by viewModel.jadwalUiState.collectAsState()
+
+                BodyHomeJadwalView(
+                    jadwalUiState = jadwalUiState,
+                    onDetailClick = { onDetailClick(it) },
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
         }
     }
 }

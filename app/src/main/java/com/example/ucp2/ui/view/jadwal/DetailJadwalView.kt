70dc4b80b090abcfdbeac85a74b2bed875c7ce67
LYChubby <f4uzi345@gmail.com>
2024-12-21 16:27:47 +0700

    Membuat Fungsi Untuk Isi Detail

--- app/src/main/java/com/example/ucp2/ui/view/jadwal/DetailJadwalView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/DetailJadwalView.kt
@@ -1,16 +1,24 @@
 package com.example.ucp2.ui.view.jadwal
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.Jadwal
 
 @Composable
 private fun DeleteConfirmationDialog(
@@ -58,5 +66,34 @@ fun ComponentDetailJadwal(
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold
         )
+    }
+}
+
+@Composable
+fun ItemDetailJadwal(
+    modifier: Modifier = Modifier,
+    jadwal: Jadwal
+) {
+    Card (
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ){
+            ComponentDetailJadwal(judul = "Nama Pasien", isinya = jadwal.namaPasien)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJadwal(judul = "Nama Dokter", isinya = jadwal.namaDokter)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJadwal(judul = "Telepon", isinya = jadwal.telepon)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJadwal(judul = "Tanggal", isinya = jadwal.tanggal)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJadwal(judul = "Status", isinya = jadwal.status)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
     }
 }

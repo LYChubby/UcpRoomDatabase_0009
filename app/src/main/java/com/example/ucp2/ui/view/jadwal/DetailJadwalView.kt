2bfbe182abb2c0e0109bef27391bd7afb81d0808
LYChubby <f4uzi345@gmail.com>
2024-12-21 16:19:13 +0700

    Membuat Konfirmasi Alert Untuk Hapus

---
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/DetailJadwalView.kt
@@ -0,0 +1,31 @@
+package com.example.ucp2.ui.view.jadwal
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = {  },
+        title = { Text(text = "Delete Data") },
+        text = { Text(text = "Apakah Anda Yakin Ingin Menghapus Data Ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Batal")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Hapus")
+            }
+        }
+    )
+}

8efd803836ae2030fef79ec3cd898085e2d72555
LYChubby <f4uzi345@gmail.com>
2024-12-21 14:26:52 +0700

    Menambah Fungsi Untuk Menyimpan Data Ke Ui

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DetailJadwalViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailJadwalViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.ucp2.ui.viewmodel
 
+import com.example.ucp2.data.entity.Jadwal
+
 data class DetailJadwalUiState(
     val detailJadwalUiState: JadwalEvent = JadwalEvent(),
     val isLoading: Boolean = false,
@@ -11,4 +13,15 @@ data class DetailJadwalUiState(
 
     val isUiJadwalEventNotEmpty: Boolean
     get() = detailJadwalUiState != JadwalEvent()
+}
+
+fun Jadwal.toDetailJadwalEvent(): JadwalEvent {
+    return JadwalEvent(
+        id = id,
+        namaPasien = namaPasien,
+        namaDokter = namaDokter,
+        telepon = telepon,
+        tanggal = tanggal,
+        status = status
+    )
 }
